04e0e85c429c100ff6c5c3b65f8bd24a3dc71cfa
key-value-pair <50742300+key-value-pair@users.noreply.github.com>
2021-01-09 23:10:32 +0800

    add transfer method implemention

--- lexgram/AutomatonStatus.cpp
+++ lexgram/AutomatonStatus.cpp
@@ -3,3 +3,7 @@
 void AutomatonStatus::addTransfer(char ch, AutomatonStatus* transferStatus) {
     transferTables_[ch].insert(transferStatus);
 }
+
+std::set<AutomatonStatus*> AutomatonStatus::transfer(char ch) {
+    return transferTables_[ch];
+}
