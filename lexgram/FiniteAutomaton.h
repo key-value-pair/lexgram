f15f9e0306469ebe89fdc3af1b9f72733c0318c0
key-value-pair <50742300+key-value-pair@users.noreply.github.com>
2021-01-09 23:50:34 +0800

    Update FiniteAutomaton.h

--- lexgram/FiniteAutomaton.h
+++ lexgram/FiniteAutomaton.h
@@ -1,3 +1,22 @@
 #ifndef FINITEAUTOMATON_H
 #define FINITEAUTOMATON_H
+
+class FiniteAutomaton {
+public:
+    void setData(char* data) {
+        data_ = data;
+    }
+    
+    void setDataFileName(const std::string& dataFileName) {
+        dataFileName_ = dataFileName;
+    }
+    
+    std::vector<Token> getTokens();
+    bool hasNextToken();
+    Token getNextToken();
+private:
+    char* data_;
+    std::string dataFileName_;
+};
+
 #endif // FINITEAUTOMATON_H
