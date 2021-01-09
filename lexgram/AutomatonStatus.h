53a4d42952829b93bd3f1673114ed48c5e0df086
key-value-pair <50742300+key-value-pair@users.noreply.github.com>
2021-01-09 22:54:01 +0800

    Update AutomatonStatus.h

--- lexgram/AutomatonStatus.h
+++ lexgram/AutomatonStatus.h
@@ -1,3 +1,16 @@
 #ifndef AUTOMATONSTATUS_H
 #define AUTOMATONSTATUS_H
+
+#include <map>
+#include <set>
+
+class AutomatonStatus {
+public:
+    void addTransfer(char ch, AutomatonStatus* transferStatus);
+    std::set<AutomatonStatus*> transfer(char ch);
+    
+private:
+    std::map<char, std::set<AutomatonStatus*>> transferTables_;
+};
+
 #endif // AUTOMATONSTATUS_H
