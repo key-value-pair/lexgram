7b59ad8ec0ca1bfeda52d465189fac0988feb611
key-value-pair <50742300+key-value-pair@users.noreply.github.com>
2021-01-10 00:17:10 +0800

    Update FiniteAutomaton.cpp

--- lexgram/FiniteAutomaton.cpp
+++ lexgram/FiniteAutomaton.cpp
@@ -1,1 +1,13 @@
 #include "FiniteAutomaton.h"
+
+void FiniteAutomaton::_buildAutomatonStatus() {
+    // TODO
+}
+
+void FiniteAutomaton::_convertNFAtoDFA() {
+    // TODO
+}
+
+void FiniteAutomaton::_minimizeDFA() {
+    // TODO
+}
