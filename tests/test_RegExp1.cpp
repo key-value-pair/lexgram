c84b54160eb096a96c2bcbff9c3edeec41ee53bf
key-value-pair <2279174697@qq.com>
2021-01-10 22:26:52 +0800

    modify tests

--- tests/test_RegExp1.cpp
+++ tests/test_RegExp1.cpp
@@ -1,45 +1,46 @@
 #include "RegExp.h"
+#include "test_tool.h"
 
-bool testRegExp1() {
+void testRegExp1() {
     RegExp regExp{"abc"};
     regExp.convertStrToRegExpNode();
     auto root = regExp.getRoot();
-    
-    if (root == nullptr) {
-        return false;
-    }
-    if (!root->nodeTypeIs(RegExpNode::Concat)) {
-        return false;
-    }
-    if (root->getSubRegExpNodes().size() != 3) {
-        return false;
-    }
-    if (root->getSubRegExpNodes()[0]->getChar() != 'a') {
-        return false;
-    }
-    if (!root->getSubRegExpNodes()[0]->nodeTypeIs(RegExpNode::Normal)) {
-        return false;
-    }
-    if (root->getSubRegExpNodes()[1]->getChar() != 'b') {
-        return false;
-    }
-    if (!root->getSubRegExpNodes()[1]->nodeTypeIs(RegExpNode::Normal)) {
-        return false;
-    }
-    if (root->getSubRegExpNodes()[2]->getChar() != 'c') {
-        return false;
-    }
-    if (!root->getSubRegExpNodes()[2]->nodeTypeIs(RegExpNode::Normal)) {
-        return false;
-    }
-    
-    return true;
+
+    AssertEqualDefaultMsg(root != nullptr);
+    AssertEqualDefaultMsg(root->nodeTypeIs(RegExpNode::Concat));
+    AssertEqualDefaultMsg(root->getSubRegExpNodes().size() == 3);
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[0]->getChar() == 'a');
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[0]->nodeTypeIs(RegExpNode::Normal));
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[1]->getChar() == 'b');
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[1]->nodeTypeIs(RegExpNode::Normal));
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[2]->getChar() == 'c');
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[2]->nodeTypeIs(RegExpNode::Normal));
 }
 
-int main(int argc, char* argv[]) {
-    if (!testRegExp1()) {
-        return 1;
-    }
-    
-    return 0;
+void testRegExp2() {
+    RegExp regExp{"a|b"};
+    regExp.convertStrToRegExpNode();
+    auto root = regExp.getRoot();
+
+    AssertEqualDefaultMsg(root != nullptr);
+    AssertEqualDefaultMsg(root->nodeTypeIs(RegExpNode::Or));
+    AssertEqualDefaultMsg(root->getSubRegExpNodes().size() == 2);
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[0]->getChar() == 'a');
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[0]->getChar() == 'b');
 }
+
+void testRegExp3() {
+    RegExp regExp{"a*b"};
+    regExp.convertStrToRegExpNode();
+    auto root = regExp.getRoot();
+
+    AssertEqualDefaultMsg(root != nullptr);
+    AssertEqualDefaultMsg(root->nodeTypeIs(RegExpNode::Concat));
+    AssertEqualDefaultMsg(root->getSubRegExpNodes().size() == 2);
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[0]->nodeTypeIs(RegExpNode::Repeated));
+    AssertEqualDefaultMsg(root->getSubRegExpNodes()[1]->getChar() == 'b');
+}
+
+ADD_TEST(testRegExp1);
+ADD_TEST(testRegExp2);
+ADD_TEST(testRegExp3);
