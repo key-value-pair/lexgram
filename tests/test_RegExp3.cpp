de74e83b7c46641f7252d50c1214beec729cbca5
key-value-pair <50742300+key-value-pair@users.noreply.github.com>
2021-01-10 19:53:02 +0800

    Create test_RegExp3.cpp

---
+++ tests/test_RegExp3.cpp
@@ -0,0 +1,28 @@
+#include "RegExp.h"
+
+int main(int argc, char* argv[]) {
+    RegExp regExp{"a*b"};
+    regExp.convertStrToRegExpNode();
+    auto root = regExp.getRoot();
+
+    if (root == nullptr) {
+        return 1;
+    }
+    if (!root->nodeTypeIs(RegExpNode::Concat)) {
+        return 2;
+    }
+    if (root->getSubRegExpNodes().size() != 2) {
+        return 3;
+    }
+    if (!root->getSubRegExpNodes()[0]->nodeTypeIs(RegExpNode::Concat)) {
+        return 4;
+    }
+    if (root->getSubRegExpNodes()[1]->getChar() != 'b') {
+        return 5;
+    }
+    if (root->getSubRegExpNodes()[1]->nodeTypeIs(RegExpNode::Normal)) {
+        return 6;
+    }
+
+    return 0;
+}
