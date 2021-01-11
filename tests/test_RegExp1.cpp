60403910a82a39de6faf2ee8f478db2f81a3b7f0
key-value-pair <2279174697@qq.com>
2021-01-11 14:28:48 +0800

    Update test_RegExp1.cpp

--- tests/test_RegExp1.cpp
+++ tests/test_RegExp1.cpp
@@ -37,8 +37,11 @@ void testRegExp3() {
     TEST_T(root != nullptr);
     TEST_T(root->nodeTypeIs(RegExpNode::Concat));
     TEST_T(root->getSubRegExpNodes().size() == 2);
-    TEST_T(root->getSubRegExpNodes()[0]->nodeTypeIs(RegExpNode::Repeated));
     TEST_T(root->getSubRegExpNodes()[1]->getChar() == 'b');
+    auto start = root->getSubRegExpNodes()[0];
+    TEST_T(start->nodeTypeIs(RegExpNode::Repeated));
+    TEST_T(start->getSubRegExpNodes().size() == 1);
+    TEST_T(start->getSubRegExpNodes()[0] == 'a');
 }
 
 void testRegExp4() {
@@ -51,7 +54,12 @@ void testRegExp4() {
     TEST_T(root->getSubRegExpNodes().size() == 2);
     TEST_T(root->getSubRegExpNodes()[0]->nodeTypeIs(RegExpNode::Normal));
     TEST_T(root->getSubRegExpNodes()[0]->getChar() == 'a');
-    TEST_T(root->getSubRegExpNodes()[1]->nodeTypeIs(RegExpNode::Repeated));
+
+    auto star = root->getSubRegExpNodes()[1];
+    TEST_T(start->nodeTypeIs(RegExpNode::Repeated));
+    TEST_T(start->getSubRegExpNodes().size() == 1);
+    TEST_T(start->getSubRegExpNodes()[0]->getChar() == 'b');
+    
 }
 
 void testRegExp5() {
@@ -67,8 +75,44 @@ void testRegExp5() {
     TEST_T(root->getSubRegExpNodes()[2]->getChar() == 'c');
 }
 
+void testRegExp6() {
+    RegExp regExp {"a|bc"};
+    regExp.convertStrToRegExpNode();
+    auto root = regExp.getRoot();
+
+    TEST_T(root != nullptr);
+    TEST_T(root->nodeTypeIs(RegExpNode::Or));
+    TEST_T(root->getSubRegExpNodes().size() == 2);
+    TEST_T(root->getSubRegExpNodes()[0]->getChar() == 'a');
+
+    auto concat = root->getSubRegExpNodes()[1];
+    TEST_T(concat->nodeTypeIs(RegExpNode::Concat));
+    TEST_T(concat->getSubRegExpNodes().size() == 2);
+    TEST_T(concat->getSubRegExpNodes()[0]->getChar() == 'b');
+    TEST_T(concat->getSubRegExpNodes()[1]->getChar() == 'c');
+}
+
+void testRegExp7() {
+    RegExp regExp {"ab|c"};
+    regExp.convertStrToRegExpNode();
+    auto root = regExp.getRoot();
+
+    TEST_T(root != nullptr);
+    TEST_T(root->nodeTypeIs(RegExpNode::Or));
+    TEST_T(root->getSubRegExpNodes().size() == 2);
+    TEST_T(root->getSubRegExpNodes()[1]->getChar() == 'c');
+    auto concat = root->getSubRegExpNodes()[0];
+    TEST_T(concat->nodeTypeIs(RegExpNode::Concat));
+
+    TEST_T(concat->getSubRegExpNodes().size() == 2);
+    TEST_T(concat->getSubRegExpNodes()[0]->getChar() == 'a');
+    TEST_T(concat->getSubRegExpNodes()[1]->getChar() == 'b');
+}
+
 ADD_TEST(testRegExp1);
 ADD_TEST(testRegExp2);
 ADD_TEST(testRegExp3);
 ADD_TEST(testRegExp4);
 ADD_TEST(testRegExp5);
+ADD_TEST(testRegExp6);
+ADD_TEST(testRegExp7);
