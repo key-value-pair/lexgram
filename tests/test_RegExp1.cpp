c556ac81d5973d9f6a7d56f5f7133f62cb8ef12d
key-value-pair <2279174697@qq.com>
2021-01-11 14:56:57 +0800

    Update test_RegExp1.cpp

--- tests/test_RegExp1.cpp
+++ tests/test_RegExp1.cpp
@@ -109,6 +109,22 @@ void testRegExp7() {
     TEST_T(concat->getSubRegExpNodes()[1]->getChar() == 'b');
 }
 
+void testRegExp8() {
+    RegExp regExp {"a*|c"};
+    NO_EXCEPTION(regExp.convertStrToRegExpNode());
+    auto root = regExp.getRoot();
+
+    TEST_T(root != nullptr);
+    TEST_T(root->nodeTypeIs(RegExpNode::Or));
+    TEST_T(root->getSubRegExpNodes().size() == 2);
+    TEST_T(root->getSubRegExpNodes()[1]->getChar() == 'c');
+    auto star = root->getSubRegExpNodes()[0];
+    TEST_T(star->nodeTypeIs(RegExpNode::Repeated));
+
+    TEST_T(concat->getSubRegExpNodes().size() == 1);
+    TEST_T(concat->getSubRegExpNodes()[0]->getChar() == 'a');
+}
+
 ADD_TEST(testRegExp1);
 ADD_TEST(testRegExp2);
 ADD_TEST(testRegExp3);
@@ -116,3 +132,4 @@ ADD_TEST(testRegExp4);
 ADD_TEST(testRegExp5);
 ADD_TEST(testRegExp6);
 ADD_TEST(testRegExp7);
+ADD_TEST(testRegExp8);
